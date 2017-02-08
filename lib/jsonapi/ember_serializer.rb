ec7dcca22668e3d7bff02cadc27f4e9910664d88
Mike Pastore <mike@oobak.org>
2017-02-07 23:50:18 -0600

    Move EmberSerializer instance methods into their own module

--- lib/jsonapi/ember_serializer.rb
+++ lib/jsonapi/ember_serializer.rb
@@ -7,29 +7,32 @@ module JSONAPI
       base.class_eval do
         include Serializer
 
-        alias_method :type_for_link, :type
+        alias type_for_link type
+        alias format_name_for_link format_name
 
-        def type
-          object.class.name.demodulize.underscore.dasherize
-        end
+        include InstanceMethods
+      end
+    end
 
-        alias_method :format_name_for_link, :format_name
+    module InstanceMethods
+      def type
+        object.class.name.demodulize.underscore.dasherize
+      end
 
-        def format_name(attribute_name)
-          attribute_name.to_s.underscore.camelize(:lower)
-        end
+      def format_name(attribute_name)
+        attribute_name.to_s.underscore.camelize(:lower)
+      end
 
-        def self_link
-          "#{base_url}/#{type_for_link}/#{id}"
-        end
+      def self_link
+        "#{base_url}/#{type_for_link}/#{id}"
+      end
 
-        def relationship_self_link(attribute_name)
-          "#{self_link}/relationships/#{format_name_for_link(attribute_name)}"
-        end
+      def relationship_self_link(attribute_name)
+        "#{self_link}/relationships/#{format_name_for_link(attribute_name)}"
+      end
 
-        def relationship_related_link(attribute_name)
-          "#{self_link}/#{format_name_for_link(attribute_name)}"
-        end
+      def relationship_related_link(attribute_name)
+        "#{self_link}/#{format_name_for_link(attribute_name)}"
       end
     end
   end
